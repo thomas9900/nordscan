11c269ea5445afac14c8630240d0b7190c3d7eef
thomas9900 <52283912+thomas9900@users.noreply.github.com>
2020-09-28 20:46:34 +0300

    c

--- js/app.js
+++ js/app.js
@@ -32,4 +32,16 @@ $(window).scroll(function() {
 });
 
 
+// contact 
+(function ($) {
+    "use strict"; // Start of use strict
 
+    // Detect when form-control inputs are not empty
+    $(".cool-b4-form .form-control").on("input", function () {
+        if ($(this).val()) {
+            $(this).addClass("hasValue");
+        } else {
+            $(this).removeClass("hasValue");
+        }
+    });
+})(jQuery); // End of use strict
