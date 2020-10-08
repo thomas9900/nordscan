54882f3dc1607f0e60c83474dccfd9477c094b70
thomas9900 <52283912+thomas9900@users.noreply.github.com>
2020-10-08 10:10:50 +0300

    c

--- mail_sending.php
+++ mail_sending.php
@@ -1,21 +1,18 @@
-<?php
- 
- $to      = 'thomas.soolaid@gmail.com';
-$subject = 'Nordscan - Contact form';
-$message = $_POST["text"];
-$headers = 'From: [' . $_POST["name"] . ', ' . $_POST["email"] . ']';
-$mail = mail($to, $subject, $message, $headers);
+<?php 
+if(isset($_POST['submit'])){
+	$to = "info@nordscan.ee"; // this is your Email address
+	$from = $_POST['email']; // this is the sender's Email address
+	$first_name = $_POST['name'];
+	$subject = "Form submission";
+	$message = $first_name  . " wrote the following:" . "\n\n" . $_POST['message'];
+	$message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
-
-if ($mail) {
-	echo ("<script type='text/javascript'>
-			window.location.href='/';
-			alert('Message sent successfully!');
-		</script>");
-} else {
-	echo ("<script type='text/javascript'>
-			window.location.href='/';
-			alert('Message not delivered.');
-		</script>");
-}
+	$headers = "From:" . $from;
+	$headers2 = "From:" . $to;
+	mail($to,$subject,$message,$headers);
+	mail($from,$message2,$headers2); // sends a copy of the message to the sender
+	echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+	// You can also use header('Location: thank_you.php'); to redirect to another page.
+	// You cannot use header and echo together. It's one or the other.
+	}
 ?>
